15d57c14ce822816e4f435c3175337ee2e5b6c3c
Gnosis <emmagab38@gmail.com>
2023-08-19 01:31:44 +0100

    commit changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,102 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+
+<!-- Mirrored from omah.dexignzone.com/xhtml/page-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 12 Aug 2023 13:28:08 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="admin, dashboard" />
+    <meta name="author" content="DexignZone" />
+    <meta name="robots" content="index, follow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Omah :  Property Admin Dashboard  Bootstrap 5 Template" />
+    <meta property="og:title" content="Omah :  Property Admin Dashboard  Bootstrap 5 Template" />
+    <meta property="og:description" content="Omah :  Property Admin Dashboard  Bootstrap 5 Template" />
+    <meta property="og:image" content="social-image.png"/>
+    <meta name="format-detection" content="telephone=no">
+    <title>{{ env('APP_NAME') }}</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <link href="{{ asset('dash/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="h-100">
+<div class="authincation h-100">
+    <div class="container h-100">
+        <div class="row justify-content-center h-100 align-items-center">
+            <div class="col-md-6">
+                <div class="authincation-content">
+                    <div class="row no-gutters">
+                        <div class="col-xl-12">
+                            <div class="auth-form">
+                                <div class="text-center mb-3">
+                                    <a href="{{ route('index')  }}">
+                                        {{--                                        <img src="images/logo-full.png" alt="">--}}
+                                        <h2 style="font-weight: bolder; color: #5e5ee5" >RESF</h2>
+                                    </a>
+                                </div>
+                                <h4 class="text-center mb-4">Sign Up</h4>
+                                <form action="{{ route('register') }}">
+                                    @csrf
+                                    @if ($errors->any())
+                                        <div class="alert alert-danger">
+                                            <ul>
+                                                @foreach ($errors->all() as $error)
+                                                    <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    @endif
+
+
+                                    <div class="form-group">
+                                        <label class="mb-1"><strong>Name</strong></label>
+                                        <input type="email" name="name" class="form-control" placeholder="fullname">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="mb-1"><strong>Email</strong></label>
+                                        <input type="email" name="email" class="form-control" placeholder="email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="mb-1"><strong>Password</strong></label>
+                                        <input type="password" name="password" class="form-control" placeholder="password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="mb-1"><strong>Confirm Password</strong></label>
+                                        <input type="password" name="password_confirmation" class="form-control" placeholder="confirm password">
+                                    </div>
+
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
+                                    </div>
+                                </form>
+                                <div class="new-account mt-3">
+                                    <p>Have an account? <a class="text-primary" href="{{ route('login') }}">Sign In</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+</div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+<!--**********************************
+    Scripts
+***********************************-->
+<!-- Required vendors -->
+<script src="{{ asset('dash/vendor/global/global.min.js') }}"></script>
+<script src="{{ asset('dash/vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('dash/js/custom.min.js') }}"></script>
+<script src="{{ asset('dash/js/deznav-init.js') }}"></script>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+</body>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+<!-- Mirrored from omah.dexignzone.com/xhtml/page-login.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 12 Aug 2023 13:28:10 GMT -->
+</html>
